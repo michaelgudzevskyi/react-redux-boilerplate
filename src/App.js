55dbb3ef2767443f1925ee899546fe1eec4d3a5e
Michael <gudzevatyi@gmail.com>
2020-08-18 13:43:33 -0300

    config eslint

--- src/App.js
+++ src/App.js
@@ -1,58 +1,47 @@
-import React, { useState } from "react";
-import { Layout, Menu } from "antd";
+import React, { useState } from 'react'
+import { Layout, Menu } from 'antd'
 import {
     MenuUnfoldOutlined,
     MenuFoldOutlined,
     UserOutlined,
     VideoCameraOutlined,
     UploadOutlined,
-} from "@ant-design/icons";
+} from '@ant-design/icons'
 
-const { Header, Sider, Content } = Layout;
+const { Header, Sider, Content } = Layout
 
 function App() {
-    const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(false)
 
-    const toggle = () => setCollapsed(!collapsed);
+    const toggle = () => setCollapsed(!collapsed)
     return (
         <Layout>
-            <Sider
-                collapsedWidth={0}
-                trigger={null}
-                collapsible
-                collapsed={collapsed}
-            >
-                <div className='logo' />
-                <Menu theme='dark' mode='inline' defaultSelectedKeys={["1"]}>
-                    <Menu.Item key='1' icon={<UserOutlined />}>
+            <Sider collapsedWidth={0} trigger={null} collapsible collapsed={collapsed}>
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1" icon={<UserOutlined />}>
                         nav 1
                     </Menu.Item>
-                    <Menu.Item key='2' icon={<VideoCameraOutlined />}>
+                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
                         nav 2
                     </Menu.Item>
-                    <Menu.Item key='3' icon={<UploadOutlined />}>
+                    <Menu.Item key="3" icon={<UploadOutlined />}>
                         nav 3
                     </Menu.Item>
                 </Menu>
             </Sider>
-            <Layout className='site-layout'>
-                <Header
-                    className='site-layout-background'
-                    style={{ padding: 0 }}
-                >
-                    {React.createElement(
-                        collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-                        {
-                            className: "trigger",
-                            onClick: toggle,
-                        },
-                    )}
+            <Layout className="site-layout">
+                <Header className="site-layout-background" style={{ padding: 0 }}>
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: 'trigger',
+                        onClick: toggle,
+                    })}
                 </Header>
                 <Content
-                    className='site-layout-background'
-                    style={{
-                        margin: "24px 16px",
-                        padding: 24,
+                  className="site-layout-background"
+                  style={{
+                        margin: '24px 16px',
+                          padding: 24,
                         minHeight: 280,
                     }}
                 >
@@ -60,7 +49,7 @@ function App() {
                 </Content>
             </Layout>
         </Layout>
-    );
+    )
 }
 
-export default App;
+export default App
